140c634ac72b1bf1f914cac7e010f621e04ca9c0
mikus3363 <273024@student.pwr.edu.pl>
2024-04-26 14:50:55 +0200

    witam3

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         RandomNumberND generator = new RandomNumberND();
-        int[] tablica;
-        tablica = generator.generateArray(0, 100, 200);
-        for(int i = 0; i < tablica.length; i++) {
-            System.out.print(tablica[i] + ", ");
-            if((i+1) % 10 == 0) System.out.println();
-        }
-
+        String ciag;
+        ciag = generator.generateArray(0, 100, 200);
+        System.out.println(ciag);
     }
     //witam z tej strony Mikołaj, porobimy razem projekt?
     //Hej tutaj Piotrus, jestem za
--- src/RandomNumberND.java
+++ src/RandomNumberND.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.*;
 
 public class RandomNumberND {
     private Random random;
@@ -7,10 +8,12 @@ public class RandomNumberND {
         this.random = new Random();
     }
 
-    public int[] generateArray(int min, int max, int size) {
+    public String generateArray(int min, int max, int size) {
         int[] array = new int[size]; //inicjalizacja tablicy zastępczej
+        String[] arrayString = new String[size]; //inicjalizacja tablicy zastępczej String
         double mean = (min + max) / 2.0; //średnia arytmetyczna
         double stdDev = (max - min) / 6.0; // odchylenie standardowe
+        String pakiet = "";
 
         for (int i = 0; i < size; i += 2) {
             double u1 = random.nextDouble(); //losowa liczba z przedziału [0,1)
@@ -24,6 +27,7 @@ public class RandomNumberND {
             // Sprawdzenie, czy x1 mieści się w zakresie [min, max]
             if (x1 >= min && x1 <= max) {
                 array[i] = (int) Math.round(x1); //jeśli tak zapis do tablicy
+                arrayString[i] = Integer.toBinaryString(array[i]);
             } else {
                 i--; //jeśli nie ponowne wygenerowanie liczby
             }
@@ -31,11 +35,15 @@ public class RandomNumberND {
             // Sprawdzenie, czy x2 mieści się w zakresie [min, max]
             if (i + 1 < size && x2 >= min && x2 <= max) {
                 array[i + 1] = (int) Math.round(x2); //jeśli tak zapis do tablicy
+                arrayString[i+1] = Integer.toBinaryString(array[i+1]);
             } else if (i + 1 < size) {
                 i--; //jeśli nie ponowne wygenerowanie liczby
             }
         }
-        return array; //zwrócenie tablicy
+        for(int i = 0; i<size; i++){
+            pakiet += arrayString[i];
+        }
+        return pakiet; //zwrócenie tablicy
     }
 
 }
